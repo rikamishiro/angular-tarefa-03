e8e9befe18129f41673d37c8c7c62c0183a0413e
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-23 12:25:20 +0000

    Configura um roteamento básico.

--- apps/front/src/app/app.routes.ts
+++ apps/front/src/app/app.routes.ts
@@ -1,4 +1,25 @@
 import { Route } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: '/sobre'
+  },
 ];
