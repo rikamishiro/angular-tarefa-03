4a419feaa4906136a70fac7d834ea74774cab98c
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-23 12:54:20 +0000

    Instala o Angular Material.

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
   ],
 };
