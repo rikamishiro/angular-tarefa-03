7c435c4d8b1f8fa6612633abdfd714386e57b016
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-23 13:33:37 +0000

    Gera `LayoutComponent`.
    - Comando:
      npx nx generate @angular/material:navigation --name=components/layout --project=front --no-interactive

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   standalone: true,
   imports: [
-    RouterModule,
+    LayoutComponent,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
