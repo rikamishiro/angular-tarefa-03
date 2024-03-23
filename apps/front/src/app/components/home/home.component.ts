651d638fb733084f153d529677ff29e0ba1d68d8
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-23 14:10:51 +0000

    Introduz responsividade no `HomeComponent`.

--- apps/front/src/app/components/home/home.component.ts
+++ apps/front/src/app/components/home/home.component.ts
@@ -1,18 +1,46 @@
 import {
-  Component,
+  Component, inject,
 } from '@angular/core';
 import {
   CommonModule,
 } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+
+interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    MatGridListModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+
+  public tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+  ];
+
+  private breakpointObserver = inject(BreakpointObserver);
+  public umaColuna$ = this.breakpointObserver.observe([
+    Breakpoints.XSmall,
+    Breakpoints.Small,
+  ]).pipe(map(breakpointState => breakpointState.matches));
+  public duasColunas$ = this.breakpointObserver.observe([
+    Breakpoints.Medium,
+  ]).pipe(map(breakpointState => breakpointState.matches));
+
 }
