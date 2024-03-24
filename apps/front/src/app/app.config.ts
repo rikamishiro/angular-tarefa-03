c8482cdce48b9716c4ccbb5d6fec81d7245d09f7
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-24 12:26:17 +0000

    Introduz `FavoritoService.getAll()`.

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 import { appRoutes } from './app.routes';
+
+export const API_BASE = new InjectionToken<string>('URL base da API');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideAnimationsAsync(),
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: API_BASE,
+      useValue: 'http://localhost:3333/api',
+    },
   ],
 };
--- apps/front/src/app/services/favorito.service.ts
+++ apps/front/src/app/services/favorito.service.ts
@@ -1,9 +1,31 @@
 import {
+  HttpClient,
+} from '@angular/common/http';
+import {
   Injectable,
+  inject,
 } from '@angular/core';
+
+import { Observable, shareReplay } from 'rxjs';
+
+import { IFavorito } from '@nx-monorepo/comum';
+
+import { API_BASE } from '../app.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoritoService {
+
+  private httpClient = inject(HttpClient);
+  private apiBase = inject(API_BASE);
+
+  public getAll(): Observable<IFavorito[]> {
+    return this.httpClient.get<IFavorito[]>(
+      `${this.apiBase}/favorito`,
+    ).pipe(
+      shareReplay(),
+    );
+  }
+
 }
