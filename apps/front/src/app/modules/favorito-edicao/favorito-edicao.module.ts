30267cf73fbd6c729d46b74d664e15a559ebd205
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-24 13:31:09 +0000

    Gera `FavoritoEdicaoModule`.

---
+++ apps/front/src/app/modules/favorito-edicao/favorito-edicao.module.ts
@@ -0,0 +1,20 @@
+import {
+  NgModule,
+} from '@angular/core';
+import {
+  CommonModule,
+} from '@angular/common';
+
+import { FavoritoEdicaoRoutingModule } from './favorito-edicao-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    FavoritoEdicaoRoutingModule,
+  ],
+})
+export class FavoritoEdicaoModule {
+}
