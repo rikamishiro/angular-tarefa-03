8b328b4f7f0d88971f1f88fb67521a996c07d589
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-04-06 15:08:00 +0000

    Declara a rota de login no front end.

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -10,12 +10,15 @@ import {
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AuthModule } from '@nx-monorepo/auth';
+
 import { appRoutes } from './app.routes';
 
 export const API_BASE = new InjectionToken<string>('URL base da API');
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(AuthModule),
     provideRouter(
       appRoutes,
       withComponentInputBinding(),
