8ae0d8a654b8fb365aa414c2b418951cd760574a
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-04-07 11:46:51 +0000

    Cria o modelo de dados do formulário de login.

--- libs/auth/src/lib/auth/auth.component.ts
+++ libs/auth/src/lib/auth/auth.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -18,4 +27,16 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+
+  private fb = inject(FormBuilder);
+  public formGroup = this.fb.group({
+    login: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  public envia(): void {
+    console.log('Enviar!', this.formGroup.value);
+  }
+
+}
